1e37c13ef9fd928c6963b988ea77b11ba2fa6dd4
84763 <84763@DESKTOP-OHS3PAE>
2016-10-12 18:10:43 +0800

    添加项目文件。

---
+++ HelloASP/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloASP.Models
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Temp = "+1s";
+            ViewBag.Text1 = "苟利国家生死以";
+            ViewBag.Text2 = "发送时间";
+
+
+            return View();
+        }
+    }
+}
---
+++ HelloASP/Models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloASP.Models
+{
+    public class Class1
+    {
+        public string c;
+        public string d;
+        public string e;
+
+    }
+}
